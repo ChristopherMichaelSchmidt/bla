a552da92a6cb9c6d0aa878badbdaa817fc49f3d3
chris <christopher.schmidt89@gmx.at>
2021-10-23 10:44:52 +0200

    Initial commit

---
+++ src/konfig.java
@@ -0,0 +1,5 @@
+public class konfig {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/übung.java
@@ -0,0 +1,9 @@
+public class übung {
+    public static void main(String[] args) {
+
+
+        String a="Christopher schmidt";
+        System.out.println(a);
+
+
+    }}
